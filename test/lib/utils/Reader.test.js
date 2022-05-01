112e8b1f6992d64d1e8cf495b419d2e0ea4b7aae
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-05-01 15:35:47 -0500

    Añadiendo proyecto

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,17 @@
+const Reader = require("../../../lib/utils/Reader");
+
+describe("Unit test Reader Class", () => {
+  test("Create a Reader object", () => {
+    const myReader = Reader.readJsonFile("explorers.json");
+    expected = [
+      {
+        name: "Woopa4",
+        githubUsername: "ajolonauta4",
+        mission: "node",
+        score: 4,
+        stacks: ["javascript"],
+      },
+    ];
+    expect(myReader).toEqual(expect.arrayContaining(expected));
+  });
+});
